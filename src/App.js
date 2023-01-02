886ac5b06ed2769b303af48cbdd5288078ffd3a7
sadu <sadamendris48@gmail.com>
2023-01-02 10:25:52 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import Nav from './Components/Nav/Nav'
+import Row from './Components/Row/Row'
+import requests from './request'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="TopRated Movies" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={requests.fetchDocumentaries}
+      />
+    </div>
+  )
+}
+
+export default App
